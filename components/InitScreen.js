43907e04a2bc32e8d8a7b2c6ac128de588022ae6
fernando.martins <fernandomartins996@gmail.com>
2020-01-14 00:30:25 +0000

    Senhas e balcões.

--- components/InitScreen.js
+++ components/InitScreen.js
@@ -1,11 +1,224 @@
- import React, { Component } from 'react';
-// import { Button, View, Text, Image } from 'react-native';
-// import { createStackNavigator, createAppContainer } from 'react-navigation';
-// // import logo from '../assets/logo.png';
+import React from 'react';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-// export default class Homescreen extends Component {
-//   render() {
-//     return (
+import Constants from 'expo-constants';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+function Item({ id, title, selected, onSelect }) {
+  return (
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+      style={[
+        styles.item,
+        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+      ]}
+    >
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+function ItemQueue({ id, title, selected, onSelect }) {
+  return (
+    <View
+    style={[styles.item,{
+      
+      flexDirection: 'row',
+      height: 100,
+      //padding: 20,
+    }]}>
+      {/* #36669b */}
+    <View style={{backgroundColor: '#408abf', flex: 0.7}}>
+      <Text style={styles.title}>Hello World!</Text>
+    </View>
+    <View style={{backgroundColor: '#ff6b00', flex: 0.4}} />
+    
+  </View> 
+    // <View
+    // style={[
+    //   styles.item,
+    //   { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+    // ]}
+    // >
+    //   <View style={{backgroundColor: 'blue', flex: 0.3}} >
+    //     <Text>Hello World!</Text>
+    //   </View>
+    //   <View style={{backgroundColor: 'red', flex: 0.5}} >
+    //     <Text style={styles.title}>{title}</Text>
+    //   </View>
+      
+      
+    // </View>
+
+    // <TouchableOpacity
+    //   onPress={() => onSelect(id)}
+    //   style={[
+    //     styles.item,
+    //     { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+    //   ]}
+    // >
+    //   <Text style={styles.title}>{title}</Text>
+    //   <Text style={styles.title}>{title}</Text>
+    // </TouchableOpacity>
+  );
+}
+
+export default function App() {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
+
+      setSelected(newSelected);
+    },
+    [selected],
+  );
+
+  return (
+   
+ 
+
+    <SafeAreaView style={styles.container}>
+         
+      {/* Tittle informations */}
+      <View
+        style={[styles.item,{
+        flexDirection: 'row',
+        height: 50,
+        borderRadius: 600
+        //padding: 20,
+      }]}>
+        <View style={[styles.header, {backgroundColor: '#36669b', flex: 0.7, borderTopLeftRadius: 5,
+          borderBottomLeftRadius: 5}]}>
+          <Text style={styles.header}>Senha</Text>
+        </View>
+        <View style={{backgroundColor: '#36669b', flex: 0.4, borderTopRightRadius: 5,
+          borderBottomRightRadius: 5}}>
+            <Text style={styles.header}>Balcão</Text>
+          </View>
+      </View> 
+
+      {/* First view */}
+      {/* <View
+        style={{
+          flexDirection: 'row',
+          height: 100,
+          padding: 20,
+        }}>
+        <View style={{backgroundColor: 'blue', flex: 0.3}} />
+        <View style={{backgroundColor: 'red', flex: 0.5}} />
+        <Text>Hello World!</Text>
+      </View> */}
+
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <Item
+            numColumns={2}
+            id={item.id}
+            title={item.title}
+            selected={!!selected.get(item.id)}
+            onSelect={onSelect}
+          />
+        )}
+        keyExtractor={item => item.id}
+        extraData={selected}
+      />
+      
+      <FlatList
+        data={DATA}
+        keyExtractor={(item, index) => item.id }
+        extraData={selected}
+        renderItem={({ item }) => (
+          <ItemQueue
+            numColumns={2}
+            id={item.id}
+            title={item.title}
+            selected={!!selected.get(item.id)}
+            onSelect={onSelect}
+          />
+        )}
+         
+      />       
+
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  item: {
+    borderColor: 'white',
+    borderRadius: 2,
+    backgroundColor: '#f9c2ff',
+    //padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+    color: 'white'
+  },
+  header: {
+    fontSize: 32,
+    alignContent: "flex-end",
+    alignItems:"center",
+    color: 'white'
+  },
+  row: {
+    flex: 1,
+    justifyContent: "space-around"
+  }
+});
+
+
+// import React, { Component } from 'react';
+// import { Button, View, Text, Image } from 'react-native';
+
+
+// export default function App() {
+//   const [selected, setSelected] = React.useState(new Map());
+
+//   const onSelect = React.useCallback(
+//     id => {
+//       const newSelected = new Map(selected);
+//       newSelected.set(id, !selected.get(id));
+
+//       setSelected(newSelected);
+//     },
+//     [selected],
+//   );
+
+//   return (
 //     <View
 //         style={{
 //           flexDirection: 'row',
@@ -16,140 +229,5 @@
 //         <View style={{backgroundColor: 'red', flex: 0.5}} />
 //         <Text>Hello World!</Text>
 //       </View>
-   
-//     )
-//   }
-// }
-
-// import React from 'react';
-// import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
-// import Constants from 'expo-constants';
-
-// const DATA = [
-//   {
-//     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-//     title: 'First Item',
-//   },
-//   {
-//     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-//     title: 'Second Item',
-//   },
-//   {
-//     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-//     title: 'Third Item',
-//   },
-// ];
-
-// function Item({ title }) {
-//   return (
-//     <View style={styles.item}>
-//       <Text style={styles.title}>{title}</Text>
-//     </View>
 //   );
 // }
-
-// export default function App() {
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <FlatList
-//         data={DATA}
-//         renderItem={({ item }) => <Item title={item.title} />}
-//         keyExtractor={item => item.id}
-//       />
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     marginTop: Constants.statusBarHeight,
-//   },
-//   item: {
-//     backgroundColor: '#f9c2ff',
-//     padding: 40,
-//     marginVertical: 8,
-//     marginHorizontal: 16,
-//   },
-//   title: {
-//     fontSize: 32,
-//   },
-// });
-
-
-
-   import { View, Text, Image, StyleSheet, SafeAreaView, ScrollView  } from 'react-native'
-   import { Card, ListItem, Button, Icon } from 'react-native-elements'
-  
-   
-
-export default function App() {
-    const users = [
-        {
-           name: 'brynn',
-           avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
-        },
-        {
-            name: 'asasa',
-            avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
-         },
-    ] 
-  return (
-    // <View>
-
-
-    <SafeAreaView>
-         <ScrollView >
-
-        <Card
-            title='Utentes - Atendimento Geral'
-            imageStyle={{margin:1}}
-            image={require('../assets/abc.png')}>
-            <Text style={{marginBottom: 10}}>
-            ddsd
-            </Text>
-            <Text style={{marginBottom: 10}}>
-            ddsd
-            </Text>
-            <Button
-            icon={<Icon name='code' color='#ffffff' />}
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW' />
-        </Card>
-
-        <Card
-            title='Utentes - Atendimento Geral'
-            imageStyle={{margin:1}}
-            image={require('../assets/abc.png')}>
-            <Text style={{marginBottom: 10}}>
-            ddsd
-            </Text>
-            <Text style={styles.titleText}>
-            123
-            </Text>
-
-      
-            <Button
-            icon={<Icon name='code' color='#ffffff' />}
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW' />
-        </Card>
-        
-      </ScrollView>
-    </SafeAreaView>
-  
-    //   </View>
-   
-  );
-}
-
-
-const styles = StyleSheet.create({
- 
-    titleText: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-  });
-
-  
